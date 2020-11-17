f1a02730e95c9a951324eb7d827e7e4785e45089
VaidaEG <grausliene.vaida@gmail.com>
2020-11-17 12:53:51 +0200

    Sum function

---
+++ js/sum.js
@@ -0,0 +1,43 @@
+// const pirmas1 = 2;
+// const antras1 = 2;
+// const suma1 = pirmas1 + antras1;
+// console.log(`${pirmas1} + ${antras1} = ${suma1}`);
+
+// const pirmas2 = 8;
+// const antras2 = 6;
+// const suma2 = pirmas2 + antras2;
+// console.log(`${pirmas2} + ${antras2} = ${suma2}`);
+
+// const pirmas3 = 9;
+// const antras3 = 2;
+// const suma3 = pirmas3 + antras3;
+// console.log(`${pirmas3} + ${antras3} = ${suma3}`);
+
+
+function sumavimas(a, b) {
+    const suma = a + b;
+    console.log(`${a} + ${b} = ${suma}`);
+}
+sumavimas(2, 5);
+sumavimas(22, 55);
+sumavimas(-8, 17);
+sumavimas(-10, 17);
+
+function pazymiuVidurkis(list) {
+    let suma = 0;
+    const kiekis = list.length;
+
+    for (let i=0; i < kiekis; i++) {
+        suma = suma + list[i];
+    }
+    const vidurkis = suma / kiekis;
+    if (kiekis === 0) {
+        console.log('Neturi pazymiu.');
+    } else {
+        console.log(`Vidurkis: ${vidurkis}.`);
+    }
+}
+pazymiuVidurkis([5, 8, 9]);
+pazymiuVidurkis([5, 8, 9, 7]);
+pazymiuVidurkis([]);
+pazymiuVidurkis([10, 10, 10, 10, 10, 10, 10]);
